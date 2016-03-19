2f2457e0d72360e1a27b6aa868ebb8b9489c048c
azurr <felipertavares@Hotmail.com>
2016-03-19 20:07:13 -0300

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -82,3 +82,63 @@ void shortestPathList(Graph* graph, int src){
  
     printSolution(dist, node_count, parent,src);
 }
+float printSingleSolution(float dist[], int n, float parent[],int src,int dest){
+    if(dist[dest] != FLT_MAX){ 
+         printf("Path found!\n\n");
+        printf("Edge            Distance         Path");
+        printf("\n%d -> %d          %f         ", src, dest, dist[dest]);
+        printPath(parent, dest);
+    }
+    else{
+        printf("There is no possible path!!\n\n");
+    }
+    
+    
+}
+
+void isThereAPath(Graph* graph, int src, int dest){
+    float** matrix = graph->matrix;
+    int node_count = graph->node_count;
+    float dist[node_count];  // Output array  
+ 
+    // "boolean" array to store whether a vertex i is included in the shortest path route
+    // path tree or shortest distance from src to i is finalized
+    bool sptSet[node_count];
+ 
+    // storing the path tree
+    float parent[node_count];
+ 
+ 
+    for (int i = 0; i < node_count; i++)
+    {
+        parent[0] = -1;
+        dist[i] = FLT_MAX;
+        sptSet[i] = 0;
+    }
+ 
+    
+    dist[src] = 0;
+ 
+   
+    for (int count = 0; count < (node_count)-1; count++)
+    {
+       
+        int u = minDistance(dist, sptSet,node_count);
+ 
+        
+        sptSet[u] = 1;
+ 
+     
+        for (int v = 0; v < node_count; v++)
+ 
+            
+            if (!sptSet[v] && matrix[u][v] &&
+                dist[u] + matrix[u][v] < dist[v])
+            {
+                parent[v]  = u;
+                dist[v] = dist[u] + matrix[u][v];
+            }  
+    }
+ 
+    printSingleSolution(dist, node_count, parent,src,dest);
+}
