10b3bbd8d524a6ab15433325354d17decae20840
PandaSafadão <victorcarneiro2009@gmail.com>
2016-03-19 14:52:05 -0300

    Changed the graph implementation to be weighted.

--- graphs.c
+++ graphs.c
@@ -2,14 +2,17 @@
 
 struct graph {
     int node_count;
-    int ** matrix;
+    float** matrix;
 };
 
 void create_graph(Graph* input_graph , int node_count) {
     
-    input_graph->matrix = calloc(node_count , sizeof(int*));
+    input_graph->matrix = calloc(node_count , sizeof(float*));
     for(int i = 0; i < node_count; i++) {
-        input_graph->matrix[i] = calloc(node_count , sizeof(int));
+        input_graph->matrix[i] = calloc(node_count , sizeof(float));
+        for(int j = 0; j < node_count; j++) {
+        	input_graph->matrix[i][j] = 0; // Initialize the values of the matrix to zero - to avoid garbage data in the graph
+        }
     }
     input_graph->node_count = node_count;
 }
@@ -23,12 +26,12 @@ void destroy_graph(Graph* input_graph) {
 }
 
 int contains_edge(Graph* input_graph , int node1 , int node2) {
-    return input_graph->matrix[node1][node2] == 1;
+    return input_graph->matrix[node1][node2] > 0;
 }
 
-void create_edge(Graph* input_graph , int node1 , int node2) {
-    input_graph->matrix[node1][node2] = 1;
-    input_graph->matrix[node2][node1] = 1;
+void create_edge(Graph* input_graph , int node1 , int node2, float weight) {
+    input_graph->matrix[node1][node2] = weight;
+    input_graph->matrix[node2][node1] = weight;
 }
 
 void remove_edge(Graph* input_graph , int node1 , int node2) {
--- graphs.h
+++ graphs.h
@@ -4,5 +4,5 @@ void create_graph(Graph* input_graph , int node_count);
 void destroy_graph(Graph* input_graph);
 
 int contains_edge(Graph* input_graph , int node1 , int node2);
-void create_edge(Graph* input_graph , int node1 , int node2);
+void create_edge(Graph* input_graph , int node1 , int node2, float weight);
 void remove_edge(Graph* input_graph , int node1 , int node2);
