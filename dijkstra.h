3535e26bc8fb4452ca874e8e670213fe66070ef6
azurr <felipertavares@Hotmail.com>
2016-03-19 20:05:39 -0300

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -4,4 +4,6 @@ int minDistance(float dist[], bool sptSet[],int V);
 void printPath(float parent[], int j);
 float printSolution(float dist[], int n, float parent[],int src);
 void shortestPathList(Graph* graph, int src);
+void isThereAPath(Graph* graph, int src, int dest);
+void printSingleSolution(float dist[], int n, float parent[],int src,int dest);
 #endif
