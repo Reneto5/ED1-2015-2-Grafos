dfea0c0ba539dd8599acb3bb5540c865626a01df
Reneto5 <reneto5@msn.com>
2016-03-19 21:18:37 -0300

    User input added, remove and add edges

---
+++ graph-user-input.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "graphs.h"
+#include "graph-user-input.h"
+
+void create_edge_option(Graph* input_graph) {
+	int edge_quantity = 0;
+	int node1 = 0;
+	int node2 = 0;
+	float weight = 0;
+	
+	printf("Digite a quanditade de arestas a serem criadas\n");
+	scanf(" %d" , &edge_quantity);
+	
+	for(int i = 0 ; i < edge_quantity ; i++) {
+		printf("Diginte as arestas e o custo de %d - (formato : vertice1 vertice2 custo)\n" , (i+1));
+		scanf(" %d%d%f" , &node1 , &node2 , &weight);
+		
+		//Verify correct user input
+		if(node1 > 0 && node2 > 0 && weight > 0 && node1 <= input_graph->node_count && node2 <= input_graph->node_count) {
+			printf("Criando aresta de peso %f entre o vertice %d e o vertice %d\n" , weight , node1 , node2);
+			create_edge(input_graph , (node1 - 1) , (node2 - 1), weight);
+		} else {
+			printf("Erro na hora de remover a aresta\n");
+			break;
+		}
+		node1 = 0;
+		node2 = 0;
+		weight = 0;
+	}
+}
+
+
+void remove_edge_option(Graph* input_graph) {
+	int edge_quantity = 0;
+	int node1 = 0;
+	int node2 = 0;
+	
+	printf("Digite a quantidade de arestas a serem removidas\n");
+	scanf("&d" , &edge_quantity);
+	
+	for(int i = 0 ; i < edge_quantity; i++) {
+		printf("Digite a aresta a ser removida de %d- (formato : vertice1 vertice2)\n" , (i+1));
+		scanf(" %d[^ ]%d" , &node1 , &node2);
+		
+		if(node1 > 0 && node2 > 0 && node1 <= input_graph->node_count && node2 <= input_graph->node_count) {
+			printf("Removendo aresta de peso %f entre o vertice %d e o vertice %d\n" , input_graph->matrix[(node1-1)][(node2-1)] , node1 , node2);
+			remove_edge(input_graph , (node1-1) , (node2-1));
+		} else {
+			printf("Erro na hora de remover a aresta\n");
+			break;
+		}
+		node1 = 0;
+		node2 = 0;
+	}
+}
---
+++ graph-user-input.h
@@ -0,0 +1,4 @@
+#include "graphs.h"
+
+void create_edge_option(Graph* input_graph);
+void remove_edge_option(Graph* input_graph);
