c3ce7ebcfaba28ffd37a6e0730688c256ae35bef
azurr <felipertavares@Hotmail.com>
2016-03-20 10:35:32 -0300

    create path options and general options

--- graph-user-input.c
+++ graph-user-input.c
@@ -4,13 +4,13 @@
 #include "graph-user-input.h"
 
 void create_edge_option(Graph* input_graph) {
-	int edge_quantity = 0;
+	int edge_quantity=0;
 	int node1 = 0;
 	int node2 = 0;
 	float weight = 0;
 	
 	printf("Digite a quanditade de arestas a serem criadas\n");
-	scanf(" %d" , &edge_quantity);
+	scanf("%d" , &edge_quantity);
 	
 	for(int i = 0 ; i < edge_quantity ; i++) {
 		printf("Diginte as arestas e o custo de %d - (formato : vertice1 vertice2 custo)\n" , (i+1));
@@ -41,19 +41,33 @@ void remove_edge_option(Graph* input_graph) {
 	
 	for(int i = 0 ; i < edge_quantity; i++) {
 		printf("Digite a aresta a ser removida de %d- (formato : vertice1 vertice2)\n" , (i+1));
-		scanf(" %d[^ ]%d" , &node1 , &node2);
+		scanf("%d %d" , &node1 , &node2);
 		
 		if(node1 > 0 && node2 > 0 && node1 <= input_graph->node_count && node2 <= input_graph->node_count) {
 			printf("Removendo aresta de peso %f entre o vertice %d e o vertice %d\n" , input_graph->matrix[(node1-1)][(node2-1)] , node1 , node2);
 			remove_edge(input_graph , (node1-1) , (node2-1));
 		} else {
-			printf("Erro na hora de remover a aresta\n");
+			printf("Erro na hora de remover a aresta.\n");
 			break;
 		}
 		node1 = 0;
 		node2 = 0;
 	}
 }
+void create_path_options(Graph* graph ){
+    int v1,v2;
+    printf("Digite o primeiro vértice\n");
+    scanf("%d",&v1);
+    printf("Digite o segundo vértice\n");
+    scanf("%d",&v2);
+    if( v1 > 0 && v2 > 0 && v2<graph->node_count && v1<graph->node_count)
+        isThereAPath(graph,v1,v2);
+        else
+        printf("Erro! Vertices devem ser positivos e menores que %d.",graph->node_count);
+     
+    
+}
+
 void create_general_options(Graph* graph){
     int flag;
 	printf("\nDigite o numero da opçao desejada, para sair do programa, digite 0.\n");
@@ -63,13 +77,18 @@ void create_general_options(Graph* graph){
 		
 			case 1:
 				create_edge_option(graph);
-				printf("Digite uma nova opção(1-5):\n");
+				printf("Digite uma nova opção(1-5) ou 0 para abortar o programa.:\n");
 				break;
 			case 2:
 				remove_edge_option(graph);
-				printf("Digite uma nova opção(1-5):\n");
+				printf("Digite uma nova opção(1-5) ou 0 para abortar o programa.:\n");
+				break;
+			case 3:
+			    create_path_options(graph);
+			    printf("\nDigite uma nova opção(1-5) ou 0 para abortar o programa.:\n");
 				break;
 	 }
-    }
+	}
     
 }
+
