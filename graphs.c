bffcb74b29411856b384feaf81d713169f32fc8c
azurr <felipertavares@Hotmail.com>
2016-03-19 22:15:34 -0300

    Check whether a graph is connected

--- graphs.c
+++ graphs.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "graphs.h"
-
+#include <limits.h>
+#define  bool int
+#include <float.h>
 void create_graph(Graph* input_graph , int node_count) {
     
     input_graph->matrix = calloc(node_count , sizeof(float*));
@@ -31,3 +33,29 @@ void remove_edge(Graph* input_graph , int node1 , int node2) {
     input_graph->matrix[node1][node2] = 0;
     input_graph->matrix[node2][node1] = 0;
 }
+/* Deph first search */
+void dfs(Graph* g,int v,bool visited[]){
+    
+    float** matrix = g->matrix;
+    visited[v] = 1;
+    for(int i = 1; i <=g->node_count ; i++){
+        if(matrix[v][i] > 0 && !visited[i]){
+            dfs(g,i,visited);
+        }
+    }
+        
+}
+ 
+int isConnected(Graph* graph,int visited[]){
+    
+	for(int i = 0 ; i <graph->node_count;i++)  {visited[i] = 0;}
+    dfs(graph,1,visited);
+    int count = 0;
+     
+    for (int i = 1; i <= graph->node_count; i++)  {
+        if (visited[i])  
+             count++;
+    }
+    return count == graph->node_count;
+}
+
